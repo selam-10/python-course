fba4131983efd2b28d5957b1c1104758779bd04c
selam-10 <selamawitalamirew9@gmail.com>
2026-07-16 03:54:49 +0300

    Updated Addis Bank project and practice exercises

--- projects/module-one/Addis Bank/account.py
+++ projects/module-one/Addis Bank/account.py
@@ -20,3 +20,33 @@ acc = Account("Almaz", "CBE-1001", 1500)
 acc.deposit(500)
 
 print(acc.balance)
+class SavingsAccount(Account):
+    def __init__(self, owner, num, balance=0, rate=0.05):
+        super().__init__(owner, num, balance)
+        self.rate = rate
+
+    def add_interest(self):
+        self.deposit(self.balance * self.rate)
+
+
+class CurrentAccount(Account):
+    def __init__(self, owner, num, balance=0, od=1000):
+        super().__init__(owner, num, balance)
+        self.overdraft = od
+        # Create a savings account
+s = SavingsAccount("Selamawit", "1001", 1000)
+
+print("Owner:", s.owner)
+print("Balance:", s.balance)
+
+s.deposit(500)
+print("After deposit:", s.balance)
+
+s.add_interest()
+print("After interest:", s.balance)
+
+# Create a current account
+c = CurrentAccount("Abel", "1002", 2000)
+
+c.withdraw(1000)
+print("Current account balance:", c.balance)
